c1667e5daebb79e39731a9f869fc81be2ad39ac6
Matthew Fasman <mfasman95@gmail.com>
2017-11-09 01:50:33 -0500

    Finished part D

---
+++ client/helper/helper.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef no-unused-vars */
+const handleError = (message) => {
+  $('#errorMessage').text(message);
+  $('#domoMessage').animate({
+    width: 'toggle',
+  }, 350);
+};
+
+const redirect = (response) => {
+  $('#domoMessage').animate({ width: 'hide' }, 350);
+  window.location = response.redirect;
+};
+
+const sendAjax = (type, action, data, success) => {
+  $.ajax({
+    cache: false,
+    type,
+    url: action,
+    data,
+    dataType: 'json',
+    success,
+    error: (xhr, status, error) => {
+      const messageObj = JSON.parse(xhr.responseText);
+      handleError(messageObj.error);
+    },
+  });
+};
